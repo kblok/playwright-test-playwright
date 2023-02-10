5154f7bff1a100bb95345cfce411a9664493e7c8
Darío Kondratiuk <dariokondratiuk@gmail.com>
2023-02-10 19:02:17 -0300

    some settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,6 +32,9 @@ export default defineConfig({
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    ignoreHTTPSErrors: true,
+    headless: true,
+
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
